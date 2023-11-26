074dd8da0e516dacb46e9270fb53317c323c1c74
Boombox2610 <137889383+Boombox2610@users.noreply.github.com>
2023-11-26 22:54:24 +0530

    Update StacksUsingLL.cpp
    MAJOR UPDATE
    Stack is now a class instead of a structure

--- LinkedList/StacksUsingLL.cpp
+++ LinkedList/StacksUsingLL.cpp
@@ -1,79 +1,91 @@
 #include <iostream>
-//stack inplementation using LL
 using namespace std;
-struct Node{
+
+// Node structure to represent a node in the stack
+struct Node {
     int value;
     Node* next;
-    Node(int val):value(val), next(NULL){};
+
+    // Constructor to initialize a node with a given value
+    Node(int val) : value(val), next(NULL) {}
 };
-struct Stack{
-    Node *top;
-    Stack(){
-        top = NULL;
-    }
-    void push(int val){
-        Node *newNode = new Node(val);
+
+class Stack {
+private:
+    Node* top; // Pointer to the top of the stack
+
+public:
+    // Constructor to initialize an empty stack
+    Stack() : top(NULL) {}
+
+    // Function to push a value onto the stack
+    void push(int val) {
+        Node* newNode = new Node(val);
         newNode->next = top;
         top = newNode;
-        cout<<"\nPushed: "<<newNode->value<<endl;
+        cout << "\nPushed: " << newNode->value << endl;
     }
-    int pop(){
-        if(top == NULL){
-            cout<<"\nMT\n";
+
+    // Function to pop a value from the stack
+    int pop() {
+        if (top == NULL) {
+            cout << "\nMT\n"; // MT stands for "Empty" (Empty stack)
             return -1;
         }
-        int poper = top->value;
-        Node *temp = top;
+        int poppedValue = top->value;
+        Node* temp = top;
         top = temp->next;
         delete temp;
-        return poper;
+        return poppedValue;
     }
-    void display(){
-        if(top == NULL){
-            cout<<"\nMT\n";
+
+    // Function to display the contents of the stack
+    void display() {
+        if (top == NULL) {
+            cout << "\nMT\n"; // MT stands for "Empty" (Empty stack)
         }
-        Node *temp = top;
-        while(temp != NULL){
-            cout<<temp->value<<" ";
+        Node* temp = top;
+        while (temp != NULL) {
+            cout << temp->value << " ";
             temp = temp->next;
         }
-        cout<<endl;
+        cout << endl;
     }
 };
 
 int main() {
-  Stack stack;
-  char choice;
+    Stack stack;
+    char choice;
 
-  do {
-    std::cout << "Menu:\n1. Push\n2. Pop\n3. Display\n4. Quit\nEnter your choice: ";
-    std::cin >> choice;
+    do {
+        cout << "Menu:\n1. Push\n2. Pop\n3. Display\n4. Quit\nEnter your choice: ";
+        cin >> choice;
 
-    switch (choice) {
-      case '1': {
-        int value;
-        std::cout << "Enter a value to push: ";
-        std::cin >> value;
-        stack.push(value);
-        break;
-      }
-      case '2': {
-        int poppedValue = stack.pop();
-        if (poppedValue != -1) {
-          std::cout << "Popped: " << poppedValue << std::endl;
+        switch (choice) {
+            case '1': {
+                int value;
+                cout << "Enter a value to push: ";
+                cin >> value;
+                stack.push(value);
+                break;
+            }
+            case '2': {
+                int poppedValue = stack.pop();
+                if (poppedValue != -1) {
+                    cout << "Popped: " << poppedValue << endl;
+                }
+                break;
+            }
+            case '3':
+                stack.display();
+                break;
+            case '4':
+                cout << "Quitting the program." << endl;
+                break;
+            default:
+                cout << "Invalid choice. Please try again." << endl;
         }
-        break;
-      }
-      case '3':
-        stack.display();
-        break;
-      case '4':
-        std::cout << "Quitting the program." << std::endl;
-        break;
-      default:
-        std::cout << "Invalid choice. Please try again." << std::endl;
-    }
-  } while (choice != '4');
+    } while (choice != '4');
 
-  return 0;
+    return 0;
 }
