4ccc41e23fc85894bf3051a485abb399372f113f
Boombox2610 <137889383+Boombox2610@users.noreply.github.com>
2023-11-26 23:49:46 +0530

    Update sorting.cpp
    MAJOR UPDATE
    Added radix

--- Sorting/sorting.cpp
+++ Sorting/sorting.cpp
@@ -41,6 +41,55 @@ void insertionSort(int *arr, int len) {
     }
 }
 
+//------------------------------------------RADIX-------------------------------
+
+int getMax(int arr[], int n) {
+    int max = arr[0];
+    for (int i = 1; i < n; i++)
+        if (arr[i] > max)
+            max = arr[i];
+    return max;
+}
+
+// Using counting sort to sort the elements based on significant places
+void countingSort(int arr[], int n, int exp) {
+    const int RANGE = 10; // Range of digits is 0 to 9
+
+    int output[n];
+    int count[RANGE] = {0};
+
+    // Count occurrences in count[]
+    for (int i = 0; i < n; i++)
+        count[(arr[i] / exp) % 10]++;
+
+    // Change count[i] so that count[i] now contains actual
+    // position of this digit in output[]
+    for (int i = 1; i < RANGE; i++)
+        count[i] += count[i - 1];
+
+    // Build the output array
+    for (int i = n - 1; i >= 0; i--) {
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        count[(arr[i] / exp) % 10]--;
+    }
+
+    // Copy the output array to arr[], so that arr[] contains sorted numbers according to the current digit
+    for (int i = 0; i < n; i++)
+        arr[i] = output[i];
+}
+
+// Radix Sort
+void radixSort(int arr[], int n) {
+    // Find the maximum number to know the number of digits
+    int max = getMax(arr, n);
+
+    // Do counting sort for every digit
+    for (int exp = 1; max / exp > 0; exp *= 10)
+        countingSort(arr, n, exp);
+}
+
+
+
 int main(){
     int len;
     cout<<"\nEnter length of array: ";
