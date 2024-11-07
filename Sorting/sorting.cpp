3d43ed9b44b84d62f4783b43c8b2cf69addeddba
Boombox2610 <137889383+Boombox2610@users.noreply.github.com>
2024-11-08 00:50:53 +0530

    Update sorting.cpp

--- Sorting/sorting.cpp
+++ Sorting/sorting.cpp
@@ -41,6 +41,45 @@ void insertionSort(int *arr, int len) {
     }
 }
 
+// ------------------ MERGE SORT --------------------------------
+void merge(vector<int>& arr, int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    // Temporary arrays
+    vector<int> L(n1), R(n2);
+
+    // Copy data
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    // Merge
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k++] = L[i++];
+        } else {
+            arr[k++] = R[j++];
+        }
+    }
+    // Copy remaining elements
+    while (i < n1)
+        arr[k++] = L[i++];
+    while (j < n2)
+        arr[k++] = R[j++];
+}
+
+void mergeSort(vector<int>& arr, int left, int right) {
+    if(left >= right)
+        return;
+    int mid = left + (right - left) / 2;
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid+1, right);
+    merge(arr, left, mid, right);
+}
+
 //------------------------------------------RADIX-------------------------------
 
 int getMax(int arr[], int n) {
